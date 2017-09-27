7f6d1005448aec903f511742d1ce38b6b59660b1
NiceAndFlowy <NiceAndFlowy@user.noreply.github.com>
2017-09-27 16:20:23 -0400

    server-side rendering

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,36 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import Header from './Header';
+import ContestPreview from './ContestPreview';
+import data from '../testData';
 
 class App extends Component {
   state = {
-    pageHeader: 'Naming Contests'
+    pageHeader: 'Naming Contests',
+    contests: this.props.initialContests
   };
+  componentDidMount() {
+    axios.get('/api/contests')
+      .then(res => {
+        this.setState({
+          contests: res.data.contests
+        });
+      })
+      .catch(console.error);
+    
+  }
   render() {
     return (
       <div className="App">
         <Header message={this.state.pageHeader} />
         <div>
-          ...
+          {this.state.contests.map(contest =>
+            <ContestPreview key={contest.id} {...contest} />
+          )}
         </div>
       </div>
     );
   }
 }
-
 
 export default App;
